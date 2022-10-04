1ddc5978dded5932728b6145d5fbfffad79ba17e
Denis Stoyanouski <vandenst@tut.by>
2022-10-04 14:55:36 +0300

    add comments to class Line

--- src/metro/Line.java
+++ src/metro/Line.java
@@ -24,19 +24,22 @@ public class Line {
         station.setNextStation(stations.get(stations.indexOf(station) + 1));
     }
 
+    //add object Station to first place - after depot
     public void addHead (Station station) {
         stations.add(1, station);
         station.setPrevStation(stations.get(0));
         station.setNextStation(stations.get(2));
-
+    //change links of previous and next stations in line
         stations.get(0).setNextStation(station);
         stations.get(2).setPrevStation(station);
     }
 
     public void remove (Station station) {
         try {
+            // change links of previous and next stations
             stations.get(stations.indexOf(station) - 1).setNextStation(stations.get(stations.indexOf(station) + 1));
             stations.get(stations.indexOf(station) + 1).setPrevStation(stations.get(stations.indexOf(station) - 1));
+            //remove station from line
             stations.remove(station);
         } catch (NullPointerException | IndexOutOfBoundsException e) {
             System.out.println("Incorrect station");
@@ -45,6 +48,7 @@ public class Line {
 
     }
 
+    // output all stations in format "previous station - station - next station"
     public void output () {
         for (int i = 1; i < stations.size() - 1; i++) {
             System.out.printf("%s - %s - %s%n", stations.get(i -1).stationName, stations.get(i).stationName, stations.get(i + 1).stationName);
