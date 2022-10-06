424bbd16d7700992f79df28aabd894d6bc84ea71
Denis Stoyanouski <vandenst@tut.by>
2022-10-06 15:04:31 +0300

    changed class Main

--- src/metro/Main.java
+++ src/metro/Main.java
@@ -1,24 +1,11 @@
 package metro;
-
 import java.io.File;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         String fileName = args[0];
         File file;
         file = new File(fileName);
-        /*Pattern pattern = Pattern.compile(".+(?<!\\.)\\.json\\b");
-        Matcher matcher = pattern.matcher(fileName);
-        if (!matcher.matches()) {
-            file = null;
-            System.out.println("Incorrect file");
-            System.exit(0);
-        } else {
-            fileName = fileName.replaceAll("\\/", "\\\\").replaceFirst("\\.", ".\\\\HyperMetro\\\\task");
-            file = new File(fileName);
-        }*/
         Engine.readFile(file);
 
 
